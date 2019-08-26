c3580d4543e0ca57e7573432894f868f0ec8844a
suzanaporto <suzanaporto02@gmail.com>
2019-08-26 15:52:48 -0300

    database support added

--- app/base/models.py
+++ app/base/models.py
@@ -1,18 +1,22 @@
 from bcrypt import gensalt, hashpw
 from flask_login import UserMixin
-from sqlalchemy import Binary, Column, Integer, String
+from sqlalchemy import Binary, Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+import datetime
 
 from app import db, login_manager
 
 
 class User(db.Model, UserMixin):
 
-    __tablename__ = 'User'
+    __tablename__ = 'user'
 
     id = Column(Integer, primary_key=True)
     username = Column(String, unique=True)
     email = Column(String, unique=True)
     password = Column(Binary)
+    country = Column(String, unique=False)
+    workflow = relationship('Workflow', backref='user', lazy='dynamic')
 
     def __init__(self, **kwargs):
         for property, value in kwargs.items():
@@ -40,3 +44,21 @@ def request_loader(request):
     username = request.form.get('username')
     user = User.query.filter_by(username=username).first()
     return user if user else None
+
+class Workflow(db.Model):
+
+    __tablename__ = 'workflow'
+    # __table_args__ = {'extend_existing': True} 
+
+    id_workflow = Column(Integer, primary_key=True)
+    created = Column(DateTime,index=True, default=datetime.datetime.utcnow)
+    user_id_user = Column(Integer, ForeignKey('user.id'))
+    step1 = Column(String(128))
+    step2 = Column(String(128))
+    step3 = Column(String(128))
+    step4 = Column(String(128))
+    step5 = Column(String(128))
+    expire = Column(DateTime)
+
+    def __repr__(self):
+        return '<Workflow {}>'.format(self.id_workflow)
--- config.py
+++ config.py
@@ -2,9 +2,20 @@ from os import environ
 
 
 class Config(object):
+
+    # Template deafult database
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
     SECRET_KEY = 'key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Created database
+    SQLALCHEMY_DATABASE_URI = 'postgresql://{}:{}@{}:{}/{}'.format(
+        environ.get('GENTELELLA_DATABASE_USER', 'postgres'),
+        environ.get('GENTELELLA_DATABASE_PASSWORD', 'unifor'),
+        environ.get('GENTELELLA_DATABASE_HOST', '127.0.0.1'),
+        environ.get('GENTELELLA_DATABASE_PORT', 5432),
+        environ.get('GENTELELLA_DATABASE_NAME', 'regulomix')
+    )
 
     # THEME SUPPORT
     #  if set then url_for('static', filename='', theme='')
@@ -18,12 +29,19 @@ class ProductionConfig(Config):
     DEBUG = False
 
     # PostgreSQL database
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://{}:{}@{}:{}/{}'.format(
+    #     environ.get('GENTELELLA_DATABASE_USER', 'gentelella'),
+    #     environ.get('GENTELELLA_DATABASE_PASSWORD', 'gentelella'),
+    #     environ.get('GENTELELLA_DATABASE_HOST', 'db'),
+    #     environ.get('GENTELELLA_DATABASE_PORT', 5432),
+    #     environ.get('GENTELELLA_DATABASE_NAME', 'gentelella')
+    # )
     SQLALCHEMY_DATABASE_URI = 'postgresql://{}:{}@{}:{}/{}'.format(
-        environ.get('GENTELELLA_DATABASE_USER', 'gentelella'),
-        environ.get('GENTELELLA_DATABASE_PASSWORD', 'gentelella'),
-        environ.get('GENTELELLA_DATABASE_HOST', 'db'),
+        environ.get('GENTELELLA_DATABASE_USER', 'postgres'),
+        environ.get('GENTELELLA_DATABASE_PASSWORD', 'unifor'),
+        environ.get('GENTELELLA_DATABASE_HOST', '127.0.0.1'),
         environ.get('GENTELELLA_DATABASE_PORT', 5432),
-        environ.get('GENTELELLA_DATABASE_NAME', 'gentelella')
+        environ.get('GENTELELLA_DATABASE_NAME', 'regulomix')
     )
 
 class DebugConfig(Config):
