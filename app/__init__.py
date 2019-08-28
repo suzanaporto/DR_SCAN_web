9b6844e4ac9114fce6851f855549712339a62610
suzanaporto <suzanaporto02@gmail.com>
2019-08-28 02:16:55 -0300

    deleting more files

--- app/__init__.py
+++ app/__init__.py
@@ -22,7 +22,8 @@ def register_extensions(app):
 
 
 def register_blueprints(app):
-    for module_name in ('base', 'forms', 'ui', 'home', 'tables', 'data', 'additional', 'base'):
+    # for module_name in ('base', 'forms', 'ui', 'home', 'tables', 'data', 'additional', 'base'):
+    for module_name in ('base', 'home'):
         module = import_module('app.{}.routes'.format(module_name))
         app.register_blueprint(module.blueprint)
 
